5013adb79ac35dfdce17dc0b3a35ea759873e698
Michael Zinoviev <mishazinoviev91@gmail.com>
2020-12-10 22:28:29 +0200

    Added fix for different window sizes of browser

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d');
 const width = 500;
 const height = 700;
-const screenWidth = window.screen.width;
+const screenWidth = window.innerWidth; 
 const canvasPosition = screenWidth / 2 - width / 2;
 const isMobile = window.matchMedia('(max-width: 600px)');
 const gameOverEl = document.createElement('div');
+
+window.addEventListener("resize", function () {
+  // Refresh the page
+  window.location.reload();
+});
 
 // Paddle
 const paddleHeight = 10;
